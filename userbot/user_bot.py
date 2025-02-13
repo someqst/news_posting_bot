58fc76133e11aa522b8dcdeb7c490f2aa2c79f9e
= <=>
2025-02-13 20:18:47 +0300

    user bot fix

--- userbot/user_bot.py
+++ userbot/user_bot.py
@@ -25,16 +25,41 @@ oi = AsyncClient(api_key=dotenv.OPENAI_KEY.get_secret_value())
 
 @client.on_message()
 async def copy_to_my_channel(_, message: Message):
-    if message.from_user:
-        if message.from_user.id == 539937958:
-            return await client.send_message(message.from_user.id, "Я работаю")
+    try:
+        if message.from_user:
+            if message.from_user.id == 539937958:
+                return await client.send_message(message.from_user.id, "Я работаю")
 
-    if str(message.chat.username) not in channels:
-        return
+        if str(message.chat.username) not in channels:
+            return
+        
+        await client.send_message(539937958, "Я получил пост")
 
-    if not message.media_group_id:
-        caption = ''
-        if message.caption:
+        if not message.media_group_id:
+            caption = ''
+            if message.caption:
+                completion = await oi.chat.completions.create(
+                model="o3-mini",
+                messages=[
+                    {"role": "developer", "content": "Ты делаешь рерайт этого текста, чтобы он сохранил смысл, но поменял вид. \
+                    Убери все упоминания чужих каналов @, если есть. Сохрани стиль написания."},
+                    {"role": "user", "content": f"Текст: {message.caption}"}
+                ]
+                )
+                caption = completion.choices[0].message.content
+                if len(caption) >= 1024:
+                    completion = await oi.chat.completions.create(
+                    model="o3-mini",
+                    messages=[
+                        {"role": "developer", "content": "Сократи этот текст, сохранив смысл"},
+                        {"role": "user", "content": f"Текст: {caption}"}
+                    ]
+                    )
+                    caption = completion.choices[0].message.content
+            return await client.copy_message(from_chat_id = message.chat.id, chat_id = 8192300340, message_id = message.id, caption=caption)
+
+        if message.media_group_id not in lm:
+            lm.append(message.media_group_id)
             completion = await oi.chat.completions.create(
             model="o3-mini",
             messages=[
@@ -42,20 +67,10 @@ async def copy_to_my_channel(_, message: Message):
                 {"role": "user", "content": f"Текст: {message.caption}"}
             ]
             )
-            caption = completion.choices[0].message.content
-        return await client.copy_message(from_chat_id = message.chat.id, chat_id = 8192300340, message_id = message.id, caption=caption)
-
-    if message.media_group_id not in lm:
-        lm.append(message.media_group_id)
-        completion = await oi.chat.completions.create(
-        model="o3-mini",
-        messages=[
-            {"role": "developer", "content": "Ты делаешь рерайт этого текста, чтобы он сохранил смысл, но поменял вид."},
-            {"role": "user", "content": f"Текст: {message.caption}"}
-        ]
-        )
-        await client.copy_media_group(from_chat_id = message.chat.id, chat_id = 8192300340, message_id = message.id, captions=completion.choices[0].message.content)
-        await client.send_message(8192300340, 'Закончилось')
+            await client.copy_media_group(from_chat_id = message.chat.id, chat_id = 8192300340, message_id = message.id, captions=completion.choices[0].message.content)
+            await client.send_message(8192300340, 'Закончилось')
+    except Exception as e:
+        await client.send_message(539937958, f'Ошибка:\n{str(e)}')
 
 
 client.run()
